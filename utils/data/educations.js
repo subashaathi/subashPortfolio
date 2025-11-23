374ce17b65c8ff4f8864fff29a94d4e8f7d30fcb
subashaathi <127607801+subashaathi@users.noreply.github.com>
2025-11-23 20:22:11 +0530

    Update education entries with new degrees and durations

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
+    title: "Master of Computer Applications",
+    duration: "2024 - 2026",
+    institution: "Anna University",
+  },
+  {
+    id: 2,
+    title: "Bachelor of Computer Science ",
     duration: "2021 - 2024",
     institution: "Nallamuthu Gounder Mahalingam Collage",
   },
   {
-    id: 2,
-    title: "Higher Secondary Certificate",
+    id: 3,
+    title: "Higher Secondary",
     duration: "2019 - 2021",
     institution: "GKN Hr Sec School",
   },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2018 - 2019",
-    institution: "GKN Hr Sec School",
-  }
 ]
